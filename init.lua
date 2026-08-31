28bb345b078d4d1285e3935627728a16dc957f21
Prithvi Rajan <prithvirajan222@gmail.com>
2026-08-31 16:47:51 -0700

    chore(nvim): install markdownlint with Mason

--- init.lua
+++ init.lua
@@ -652,7 +652,7 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        -- You can add other tools here that you want Mason to install
+        'markdownlint',
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
